111cc0e78e8201e6118ca8ebee226a7cf3d34462
Jesse Luehrs <doy@tozt.net>
2009-05-14 22:23:13 -0500

    solution for 19

---
+++ 019.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use DateTime;
+
+my $day = DateTime->new(year => 1901, month => 1, day => 1);
+my $count = 0;
+while ($day->year < 2001) {
+    $count++ if $day->day_of_week == 7 && $day->day_of_month == 1;
+    $day->add(days => 1);
+}
+print "$count\n";
